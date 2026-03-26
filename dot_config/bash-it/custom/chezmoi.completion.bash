9c4ad96ae4591465af4ad67449fe130db81f112b
Jimmy Harris <james.harris@garmin.com>
2026-03-26 14:32:47 -0500

    add cz completion in chezmoi

--- dot_config/bash-it/custom/chezmoi.completion.bash
+++ dot_config/bash-it/custom/chezmoi.completion.bash
@@ -4,5 +4,10 @@ about-plugin 'alias for chezmoi for easier use'
 
 if type chezmoi &> /dev/null; then
   eval "$(chezmoi completion bash)"
+  if [[ $(type -t compopt) = "builtin" ]]; then
+    complete -o default -F __start_chezmoi cz
+  else
+      complete -o default -o nospace -F __start_chezmoi cz
+  fi
 fi
 
