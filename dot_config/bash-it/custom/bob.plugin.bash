b8eb20259ac897dcf89449a106c4843b2b9960d7
Jimmy Harris <james.harris@garmin.com>
2026-04-10 17:37:37 -0500

    fix bob.plugin.bash to fix bash completion

--- dot_config/bash-it/custom/bob.plugin.bash
+++ dot_config/bash-it/custom/bob.plugin.bash
@@ -5,9 +5,9 @@ about-plugin 'add uv tool paths to path if they exist'
 if type bob &> /dev/null; then
   pathmunge "$HOME/.local/share/bob/nvim-bin"
   _nvim_012_path="$HOME/.local/share/bob/v0.12.1"
-  if [ -f "$_nvim_012_path" ]; then
+  if [ -d "$_nvim_012_path" ]; then
     nvim_012() {
-      NVIM_APPNAME=nvim-next "$_nvim_012_path/bin/nvim" $@
+      NVIM_APPNAME=nvim-next "$_nvim_012_path/bin/nvim" "$@"
     }
   fi
 fi
