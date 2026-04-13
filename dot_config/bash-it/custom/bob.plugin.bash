99f02389e5b675aefadcddab7de673afc7185e06
Jimmy Harris <james.harris@garmin.com>
2026-04-13 13:34:04 -0500

    bob.plugin.bash: use bob run instead of path to bob nvim

--- dot_config/bash-it/custom/bob.plugin.bash
+++ dot_config/bash-it/custom/bob.plugin.bash
@@ -4,16 +4,14 @@ about-plugin 'add uv tool paths to path if they exist'
 
 if type bob &> /dev/null; then
   pathmunge "$HOME/.local/share/bob/nvim-bin"
-  _nvim_012_path="$HOME/.local/share/bob/v0.12.1"
   if [ -d "$_nvim_012_path" ]; then
-    nvim_012() {
-      NVIM_APPNAME=nvim-next "$_nvim_012_path/bin/nvim" "$@"
+    nvim-012() {
+      NVIM_APPNAME=nvim-next bob run 0.12.1 "$@"
     }
   fi
-  _nvim_011_path="$HOME/.local/share/bob/v0.11.6"
   if [ -d "$_nvim_011_path" ]; then
-    nvim_011() {
-      NVIM_APPNAME=nvim-011 "$_nvim_011_path/bin/nvim" "$@"
+    nvim-011() {
+      NVIM_APPNAME=nvim-011 bob run 0.11.6 "$@"
     }
   fi
 fi
