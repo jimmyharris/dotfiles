3448840b4edfc9d6304b6ece6c43cf503c31ec18
Jimmy Harris <james.harris@garmin.com>
2026-04-13 13:29:30 -0500

    Create nvim_011 to manage my nvim_config app-name
    Renames the .config/nvim to .config/nvim-011 and symlink's .config/nvim
    to .config/nvim-011. This will make the transition to 0.12 easier for me
    in the long run.

--- dot_config/bash-it/custom/bob.plugin.bash
+++ dot_config/bash-it/custom/bob.plugin.bash
@@ -10,4 +10,10 @@ if type bob &> /dev/null; then
       NVIM_APPNAME=nvim-next "$_nvim_012_path/bin/nvim" "$@"
     }
   fi
+  _nvim_011_path="$HOME/.local/share/bob/v0.11.6"
+  if [ -d "$_nvim_011_path" ]; then
+    nvim_011() {
+      NVIM_APPNAME=nvim-011 "$_nvim_011_path/bin/nvim" "$@"
+    }
+  fi
 fi
--- dot_config/nvim/local/overrides/README.md
+++
@@ -1,2 +0,0 @@
-This folder can be used for system local plugin scripts that won't be tracked
-in this repository. It is treated as a single plugin by vim-plug.
