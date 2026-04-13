5f11b4c61d5a38f94156939c4879a936c4f84e7a
Jimmy Harris <james.harris@garmin.com>
2026-04-13 13:47:34 -0500

    fix bob.plugin.bash to always add nvim version arguments

--- dot_config/bash-it/custom/bob.plugin.bash
+++ dot_config/bash-it/custom/bob.plugin.bash
@@ -4,14 +4,10 @@ about-plugin 'add uv tool paths to path if they exist'
 
 if type bob &> /dev/null; then
   pathmunge "$HOME/.local/share/bob/nvim-bin"
-  if [ -d "$_nvim_012_path" ]; then
-    nvim-012() {
-      NVIM_APPNAME=nvim-012 bob run 0.12.1 "$@"
-    }
-  fi
-  if [ -d "$_nvim_011_path" ]; then
-    nvim-011() {
-      NVIM_APPNAME=nvim-011 bob run 0.11.6 "$@"
-    }
-  fi
+  nvim-012() {
+    NVIM_APPNAME=nvim-012 bob run 0.12.1 "$@"
+  }
+  nvim-011() {
+    NVIM_APPNAME=nvim-011 bob run 0.11.6 "$@"
+  }
 fi
