77eb1324900d1ce0288a0cac6c03e17394bcc12b
Jimmy Harris <james.harris@garmin.com>
2026-04-13 13:38:17 -0500

    move nvim-next to nvim-012 for nvim version 0.12.1

--- dot_config/bash-it/custom/bob.plugin.bash
+++ dot_config/bash-it/custom/bob.plugin.bash
@@ -6,7 +6,7 @@ if type bob &> /dev/null; then
   pathmunge "$HOME/.local/share/bob/nvim-bin"
   if [ -d "$_nvim_012_path" ]; then
     nvim-012() {
-      NVIM_APPNAME=nvim-next bob run 0.12.1 "$@"
+      NVIM_APPNAME=nvim-012 bob run 0.12.1 "$@"
     }
   fi
   if [ -d "$_nvim_011_path" ]; then
