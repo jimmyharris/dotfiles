6edddba69dde6ba9aef35cae55bac03ef6ad4833
Jimmy Harris <james.harris@garmin.com>
2026-04-16 13:38:18 -0500

    nvim-config: direct-link nvim-config from chezmoi source for reasier development
    This patch also drops the tiny-cmdline plugin for nvim-012 and makes the
    default nvim version 0.12.1 on all platforms.

---
+++ nvim-config/nvim-012/plugin/dashboard-nvim.lua
@@ -0,0 +1,78 @@
+-- Do not lazy load this plugin!
+vim.pack.add {
+  { src = "https://github.com/nvimdev/dashboard-nvim" },
+}
+local api = vim.api
+local keymap = vim.keymap
+local dashboard = require("dashboard")
+
+local conf = {}
+conf.header = {
+  "                                                       ",
+  "                                                       ",
+  "                                                       ",
+  " ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
+  " ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
+  " ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
+  " ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
+  " ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
+  " ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
+  "                                                       ",
+  "                                                       ",
+  "                                                       ",
+  "                                                       ",
+}
+
+conf.center = {
+  {
+    icon = "󰈞  ",
+    desc = "Find  File                              ",
+    action = "lua require('telescope.builtin').find_files()",
+    key = "<Leader> f f",
+  },
+  {
+    icon = "󰈢  ",
+    desc = "Recently opened files                   ",
+    action = "lua require('telescope.builtin').oldfiles()",
+    key = "<Leader> f r",
+  },
+  {
+    icon = "󰈬  ",
+    desc = "Project grep                            ",
+    action = "lua require('telescope').extensions.live_grep_args.live_grep_args()",
+    key = "<Leader> f g",
+  },
+  {
+    icon = "  ",
+    desc = "Open Nvim config                        ",
+    action = "tabnew $MYVIMRC | tcd %:p:h",
+    key = "<Leader> e v",
+  },
+  {
+    icon = "  ",
+    desc = "New file                                ",
+    action = "enew",
+    key = "e",
+  },
+  {
+    icon = "󰗼  ",
+    desc = "Quit Nvim                               ",
+    action = "qa",
+    key = "q",
+  },
+}
+
+dashboard.setup {
+  theme = "doom",
+  shortcut_type = "number",
+  config = conf,
+}
+
+api.nvim_create_autocmd("FileType", {
+  pattern = "dashboard",
+  group = api.nvim_create_augroup("dashboard_enter", { clear = true }),
+  callback = function()
+    keymap.set("n", "q", ":qa<CR>", { buffer = true, silent = true })
+    keymap.set("n", "e", ":enew<CR>", { buffer = true, silent = true })
+  end,
+})
--- nvim-config/nvim-012/plugin/fzf-lua.lua
+++ nvim-config/nvim-012/plugin/fzf-lua.lua
@@ -2,7 +2,7 @@ require("lazyload").on_vim_enter(function()
   -- Requires webdev-icons or mini.icons
   vim.pack.add {
     {
-      src = __.gh("ibhagwan/fzf-lua"),
+      src = "https://github.com/ibhagwan/fzf-lua",
     },
   }
 
