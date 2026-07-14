39384a5326a335d1cf7a941ef75ab1a62a57252b
Jimmy Harris <james.harris@garmin.com>
2026-07-14 15:18:36 -0500

    nvim-012: use uv when installing from pypi

--- nvim-config/nvim-012/plugin/mason-lspconfig-conf.lua
+++ nvim-config/nvim-012/plugin/mason-lspconfig-conf.lua
@@ -14,6 +14,46 @@ require("lazyload").on_vim_enter(function()
 
   require("mason").setup()
 
+  -- Allow us to use uv for pypi lsps
+  local Result = require "mason-core.result"
+  local path = require "mason-core.path"
+  local compiler = require "mason-core.installer.compiler"
+  local pypi = require "mason-core.installer.compiler.compilers.pypi"
+
+  local extend = { __index = pypi }
+
+  local uv = {
+    ---@async
+    ---@param ctx InstallContext
+    ---@param source ParsedPypiSource
+    install = function(ctx, source)
+      -- Note that this implementation will always try to use uv and not fall back to default.
+      -- Uncomment the following for a default fallback.
+      if utils.executable("uv") then
+        return pypi.install(ctx, source)
+      end
+      return Result.try(function(try)
+        ctx:promote_cwd()
+        try(ctx.spawn.uv({ "venv", "venv" }))
+        try(ctx.spawn.uv({
+          "pip",
+          "install",
+          source.extra and ("%s[%s]==%s"):format(source.package, source.extra, source.version)
+          or ("%s==%s"):format(source.package, source.version),
+          source.extra_packages or vim.NIL,
+          env = {
+            VIRTUAL_ENV = path.concat { ctx.cwd:get(), "venv" },
+          },
+        }))
+      end)
+    end,
+  }
+
+  setmetatable(uv, extend)
+
+  compiler.register_compiler("pypi", uv)
+
+  -- List all lsp servers we want to include
   local npm_based_lsps = {
     "ansiblels",
     "bashls",
