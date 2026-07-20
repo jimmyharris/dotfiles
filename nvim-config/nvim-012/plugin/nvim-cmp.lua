6244961f4985751a5ee5ef65518b16389f8e57a9
Jimmy Harris <james.harris@garmin.com>
2026-07-20 12:30:30 -0500

    nvim-012: remove dependency on ultisnips
    Hidden dependency on ultisnips breaks completion in some files. This
    patch nukes it from orbit. (use neovim native vim.snippets instead)
    
    I don't use snippets anyway so it was breaking me to do this.

--- nvim-config/nvim-012/plugin/nvim-cmp.lua
+++ nvim-config/nvim-012/plugin/nvim-cmp.lua
@@ -39,8 +39,8 @@ require("lazyload").on_vim_enter(function()
   cmp.setup {
     snippet = {
       expand = function(args)
-        -- For `ultisnips` user.
-        vim.fn["UltiSnips#Anon"](args.body)
+        -- For native neovim snippets users
+        vim.snippet.expand(args.body)
       end,
     },
     mapping = cmp.mapping.preset.insert {
@@ -66,7 +66,6 @@ require("lazyload").on_vim_enter(function()
     },
     sources = {
       { name = "nvim_lsp" }, -- For nvim-lsp
-      { name = "ultisnips" }, -- For ultisnips user.
       { name = "path" }, -- for path completion
       { name = "buffer", keyword_length = 2 }, -- for buffer word completion
     },
@@ -91,7 +90,132 @@ require("lazyload").on_vim_enter(function()
   cmp.setup.filetype("tex", {
     sources = {
       { name = "omni" },
-      { name = "ultisnips" }, -- For ultisnips user.
+      { name = "buffer", keyword_length = 2 }, -- for buffer word completion
+      { name = "path" }, -- for path completion
+    },
+  })
+
+  cmp.setup.cmdline("/", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = "buffer" },
+    },
+  })
+
+  cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = "path" },
+    }, {
+      { name = "cmdline" },
+    }),
+    matching = { disallow_symbol_nonprefix_matching = false },
+  })
+
+  --  see https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance#how-to-add-visual-studio-code-dark-theme-colors-to-the-menu
+  vim.cmd([[
+  highlight! link CmpItemMenu Comment
+  " gray
+  highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080
+  " blue
+  highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6
+  highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6
+  " light blue
+  highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE
+  highlight! CmpItemKindInterface guibg=NONE guifg=#9CDCFE
+  highlight! CmpItemKindText guibg=NONE guifg=#9CDCFE
+  " pink
+  highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0
+  highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0
+  " front
+  highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4
+  highlight! CmpItemKindProperty guibg=NONE guifg=#D4D4D4
+  highlight! CmpItemKindUnit guibg=NONE guifg=#D4D4D4
+  ]])
+  -- Setup nvim-cmp.
+  vim.pack.add {
+    {
+      src = "https://github.com/hrsh7th/cmp-nvim-lsp",
+    },
+    {
+      src = "https://github.com/onsails/lspkind-nvim",
+    },
+    {
+      src = "https://github.com/hrsh7th/cmp-path",
+    },
+    {
+      src = "https://github.com/hrsh7th/cmp-buffer",
+    },
+    {
+      src = "https://github.com/hrsh7th/cmp-omni",
+    },
+    {
+      src = "https://github.com/hrsh7th/cmp-cmdline",
+    },
+    {
+      src = "https://github.com/hrsh7th/nvim-cmp",
+    },
+  }
+  -- Setup nvim-cmp.
+  local cmp = require("cmp")
+
+  -- The extentions needed by nvim-cmp should be loaded beforehand
+  require("cmp_nvim_lsp")
+  require("cmp_path")
+  require("cmp_buffer")
+  require("cmp_omni")
+  require("cmp_cmdline")
+
+  local MiniIcons = require("mini.icons")
+
+  cmp.setup {
+    mapping = cmp.mapping.preset.insert {
+      ["<Tab>"] = function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        else
+          fallback()
+        end
+      end,
+      ["<S-Tab>"] = function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
+      end,
+      ["<CR>"] = cmp.mapping.confirm { select = true },
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<Esc>"] = cmp.mapping.close(),
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    },
+    sources = {
+      { name = "nvim_lsp" }, -- For nvim-lsp
+      { name = "path" }, -- for path completion
+      { name = "buffer", keyword_length = 2 }, -- for buffer word completion
+    },
+    completion = {
+      keyword_length = 1,
+      completeopt = "menu,noselect",
+    },
+    view = {
+      entries = "custom",
+    },
+    -- solution taken from https://github.com/echasnovski/mini.nvim/issues/1007#issuecomment-2258929830
+    formatting = {
+      format = function(_, vim_item)
+        local icon, hl = MiniIcons.get("lsp", vim_item.kind)
+        vim_item.kind = icon .. " " .. vim_item.kind
+        vim_item.kind_hl_group = hl
+        return vim_item
+      end,
+    },
+  }
+
+  cmp.setup.filetype("tex", {
+    sources = {
+      { name = "omni" },
       { name = "buffer", keyword_length = 2 }, -- for buffer word completion
       { name = "path" }, -- for path completion
     },
@@ -173,8 +297,8 @@ require("lazyload").on_vim_enter(function()
   cmp.setup {
     snippet = {
       expand = function(args)
-        -- For `ultisnips` user.
-        vim.fn["UltiSnips#Anon"](args.body)
+        -- Native neovim snippet support
+        vim.snippet.expand(args.body)
       end,
     },
     mapping = cmp.mapping.preset.insert {
@@ -200,7 +324,6 @@ require("lazyload").on_vim_enter(function()
     },
     sources = {
       { name = "nvim_lsp" }, -- For nvim-lsp
-      { name = "ultisnips" }, -- For ultisnips user.
       { name = "path" }, -- for path completion
       { name = "buffer", keyword_length = 2 }, -- for buffer word completion
     },
@@ -225,141 +348,6 @@ require("lazyload").on_vim_enter(function()
   cmp.setup.filetype("tex", {
     sources = {
       { name = "omni" },
-      { name = "ultisnips" }, -- For ultisnips user.
-      { name = "buffer", keyword_length = 2 }, -- for buffer word completion
-      { name = "path" }, -- for path completion
-    },
-  })
-
-  cmp.setup.cmdline("/", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = "buffer" },
-    },
-  })
-
-  cmp.setup.cmdline(":", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = "path" },
-    }, {
-      { name = "cmdline" },
-    }),
-    matching = { disallow_symbol_nonprefix_matching = false },
-  })
-
-  --  see https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance#how-to-add-visual-studio-code-dark-theme-colors-to-the-menu
-  vim.cmd([[
-  highlight! link CmpItemMenu Comment
-  " gray
-  highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080
-  " blue
-  highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6
-  highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6
-  " light blue
-  highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE
-  highlight! CmpItemKindInterface guibg=NONE guifg=#9CDCFE
-  highlight! CmpItemKindText guibg=NONE guifg=#9CDCFE
-  " pink
-  highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0
-  highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0
-  " front
-  highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4
-  highlight! CmpItemKindProperty guibg=NONE guifg=#D4D4D4
-  highlight! CmpItemKindUnit guibg=NONE guifg=#D4D4D4
-  ]])
-  -- Setup nvim-cmp.
-  vim.pack.add {
-    {
-      src = "https://github.com/hrsh7th/cmp-nvim-lsp",
-    },
-    {
-      src = "https://github.com/onsails/lspkind-nvim",
-    },
-    {
-      src = "https://github.com/hrsh7th/cmp-path",
-    },
-    {
-      src = "https://github.com/hrsh7th/cmp-buffer",
-    },
-    {
-      src = "https://github.com/hrsh7th/cmp-omni",
-    },
-    {
-      src = "https://github.com/hrsh7th/cmp-cmdline",
-    },
-    {
-      src = "https://github.com/hrsh7th/nvim-cmp",
-    },
-  }
-  -- Setup nvim-cmp.
-  local cmp = require("cmp")
-
-  -- The extentions needed by nvim-cmp should be loaded beforehand
-  require("cmp_nvim_lsp")
-  require("cmp_path")
-  require("cmp_buffer")
-  require("cmp_omni")
-  require("cmp_cmdline")
-
-  local MiniIcons = require("mini.icons")
-
-  cmp.setup {
-    snippet = {
-      expand = function(args)
-        -- For `ultisnips` user.
-        vim.fn["UltiSnips#Anon"](args.body)
-      end,
-    },
-    mapping = cmp.mapping.preset.insert {
-      ["<Tab>"] = function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        else
-          fallback()
-        end
-      end,
-      ["<S-Tab>"] = function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        else
-          fallback()
-        end
-      end,
-      ["<CR>"] = cmp.mapping.confirm { select = true },
-      ["<C-e>"] = cmp.mapping.abort(),
-      ["<Esc>"] = cmp.mapping.close(),
-      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    },
-    sources = {
-      { name = "nvim_lsp" }, -- For nvim-lsp
-      { name = "ultisnips" }, -- For ultisnips user.
-      { name = "path" }, -- for path completion
-      { name = "buffer", keyword_length = 2 }, -- for buffer word completion
-    },
-    completion = {
-      keyword_length = 1,
-      completeopt = "menu,noselect",
-    },
-    view = {
-      entries = "custom",
-    },
-    -- solution taken from https://github.com/echasnovski/mini.nvim/issues/1007#issuecomment-2258929830
-    formatting = {
-      format = function(_, vim_item)
-        local icon, hl = MiniIcons.get("lsp", vim_item.kind)
-        vim_item.kind = icon .. " " .. vim_item.kind
-        vim_item.kind_hl_group = hl
-        return vim_item
-      end,
-    },
-  }
-
-  cmp.setup.filetype("tex", {
-    sources = {
-      { name = "omni" },
-      { name = "ultisnips" }, -- For ultisnips user.
       { name = "buffer", keyword_length = 2 }, -- for buffer word completion
       { name = "path" }, -- for path completion
     },
