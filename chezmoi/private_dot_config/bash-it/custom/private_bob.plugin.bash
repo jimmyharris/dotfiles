afaaac8fe3480a811964b2244d759d80c485a9f8
James Harris <james.harris@garmin.com>
2026-08-24 09:23:08 -0500

    cz: bash-it: bob plugin: bump the nvim-012 alias to 0.12.4
    This patch bumps the nvim-012 alias to use the same version of nvim bob
    is using locally.

--- chezmoi/private_dot_config/bash-it/custom/private_bob.plugin.bash
+++ chezmoi/private_dot_config/bash-it/custom/private_bob.plugin.bash
@@ -5,7 +5,7 @@ about-plugin 'add uv tool paths to path if they exist'
 if type bob &> /dev/null; then
   pathmunge "$HOME/.local/share/bob/nvim-bin"
   nvim-012() {
-    NVIM_APPNAME=nvim-012 bob run 0.12.1 "$@"
+    NVIM_APPNAME=nvim-012 bob run 0.12.4 "$@"
   }
 
   nvim-011() {
