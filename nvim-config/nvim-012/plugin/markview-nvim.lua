92ed3c43ac35e1664f91782668bd2898e1c0dc76
James Harris <james.harris@garmin.com>
2026-09-01 15:26:19 -0500

    nvim-012: add markview with side toggle to avoid automatic preview

---
+++ nvim-config/nvim-012/plugin/markview-nvim.lua
@@ -0,0 +1,18 @@
+
+require("lazyload").on_vim_enter(function()
+  vim.pack.add {
+    { src = "https://github.com/OXY2DEV/markview.nvim" },
+  }
+
+  require("markview").setup {
+    preview = {
+      enable = false,
+    },
+  }
+  -- disable preview for already open buffers.
+  require("markview").commands.Disable()
+
+  vim.api.nvim_set_keymap("n", "<leader>sv", "<CMD>Markview splitToggle<CR>", {
+    desc = "Toggles `splitview` for current buffer."
+  })
+end)
